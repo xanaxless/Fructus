7b6ea22e94e3586e1ea02d0579a3740ac6d63044
Yerkebulan Serikov <serikov.2003.18@gmail.com>
2022-04-19 17:18:45 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Yerkebulan Serikov on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
